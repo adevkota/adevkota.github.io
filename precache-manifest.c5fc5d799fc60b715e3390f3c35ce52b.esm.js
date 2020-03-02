84d25515b8c2afffc0fba1f7425b403b2aed0ffd
Tosh <adevkota@pamperedchef.com>
2020-03-02 00:46:32 -0600

    Updates

---
+++ precache-manifest.c5fc5d799fc60b715e3390f3c35ce52b.esm.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "a5877a4301337a2c2220",
+    "url": "/bundle.90112.esm.js"
+  },
+  {
+    "revision": "a5877a4301337a2c2220",
+    "url": "/bundle.9077b.css"
+  },
+  {
+    "revision": "24bf58d3023aa08cbdeedee3533eb67f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb23adb3c9bfbb7f047f",
+    "url": "/polyfills.06b13.esm.js"
+  },
+  {
+    "revision": "5eb2db91c7c707704fbe",
+    "url": "/route-home.chunk.1f94a.css"
+  },
+  {
+    "revision": "5eb2db91c7c707704fbe",
+    "url": "/route-home.chunk.fc937.esm.js"
+  },
+  {
+    "revision": "8a4f4024230474ba01b7",
+    "url": "/route-resume.chunk.7c6d4.esm.js"
+  },
+  {
+    "revision": "8a4f4024230474ba01b7",
+    "url": "/route-resume.chunk.c4175.css"
+  }
+]);
