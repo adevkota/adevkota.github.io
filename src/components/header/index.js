e3b939b21fb272068ca9d10878f9482de1c770fa
Tosh <adevkota@pamperedchef.com>
2020-03-01 21:50:53 -0600

    mock existing

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,12 +4,16 @@ import style from './style.css';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>Preact App</h1>
-		<nav>
+		{/* <nav>
 			<Link activeClassName={style.active} href="/">Home</Link>
 			<Link activeClassName={style.active} href="/profile">Me</Link>
 			<Link activeClassName={style.active} href="/profile/john">John</Link>
-		</nav>
+		</nav> */}
+		<div class={style.summary}>
+			
+			<h1 class={style.name}>Ashutosh Devkota</h1>
+			<h2 class={style.skills}>Front-End/Javascript Engineer</h2>
+		</div>
 	</header>
 );
 
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,8 +3,23 @@ import style from './style';
 
 const Home = () => (
 	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
+    <section class="intro-text">
+      <p>
+        Full stack Javascript engineer with a focus on front end side of things.
+      </p>
+      <p>
+        My current interest is in building performant web apps that takes into account the principles of Human-Computer Interaction.
+      </p>
+      <p>
+        Connect with me on  <a target="_self"href="https://www.linkedin.com/in/tosh-sharma">LinkedIn</a>.
+      </p>
+      <p>
+        Find me on <a target="_self"href="https://github.com/adevkota">GitHub</a>.
+      </p>
+      <p>
+        View/download my <a target="_self" href="https://adevkota.me/resume/">resume(pdf)</a>.
+      </p>
+    </section>
 	</div>
 );
 
