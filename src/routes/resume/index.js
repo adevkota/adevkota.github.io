9345a2719870a7b48b0fd0ccbc8c7142c2920ab1
Tosh <adevkota@pamperedchef.com>
2020-03-02 00:29:20 -0600

    more things

--- src/routes/resume/index.js
+++ src/routes/resume/index.js
@@ -4,7 +4,7 @@ import style from './style';
 const Resume = () => {
    return (
       <div class={style.resumeWrapper}>
-         <iframe src="https://docs.google.com/viewer?embedded=true&url=https://adevkota.me/resume/Ashutosh%20Devkota.pdf" frameborder="0" style="width:100%; height:100%;border:0;"/>
+         <iframe src="https://adevkota.me/resume/" frameborder="0" style="width:100%; height:100%;border:0;"/>
       </div>
    )
 }
