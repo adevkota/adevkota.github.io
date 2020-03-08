b725bab8c9cb5e384177de2843e6499f6c61448d
Tosh <adevkota@pamperedchef.com>
2020-03-08 14:37:59 -0500

    Updates

---
+++ precache-manifest.83416596826beb2dfa3e7a795a5c25cc.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "1b03c969964e059fa46d73463685dd91",
+    "url": "/assets/portfolio/pampered_chef/1.png"
+  },
+  {
+    "revision": "0bd96a7396139bd305f6b1eca9706bd0",
+    "url": "/assets/portfolio/pampered_chef/2.png"
+  },
+  {
+    "revision": "c48d3e3a47681eaf2e83352580f7d053",
+    "url": "/assets/portfolio/pampered_chef/3.png"
+  },
+  {
+    "revision": "c7775209e4a25eb77d1a7f0f4dc11310",
+    "url": "/assets/portfolio/pampered_chef/4.png"
+  },
+  {
+    "revision": "0eb3fe636c51645ad400fd47563ea1ab",
+    "url": "/assets/portfolio/pampered_chef/5.png"
+  },
+  {
+    "revision": "e71438b074c65e0bfbb95b96fc31541a",
+    "url": "/assets/portfolio/rrd/1.png"
+  },
+  {
+    "revision": "cdcd5fd5d9e8fe0e3d700fb11e748b42",
+    "url": "/assets/portfolio/rrd/2.png"
+  },
+  {
+    "revision": "5b4053b6c26ef9bc74e8056a09d9e2e0",
+    "url": "/assets/portfolio/rrd/3.png"
+  },
+  {
+    "revision": "3aeb248229b43e576473",
+    "url": "/bundle.3aeb2.js"
+  },
+  {
+    "revision": "3aeb248229b43e576473",
+    "url": "/bundle.9f062.css"
+  },
+  {
+    "revision": "7fad19114d243f019daabca7bf0dadb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb23adb3c9bfbb7f047f",
+    "url": "/polyfills.bb23a.js"
+  },
+  {
+    "revision": "61f2cd78d19fcd0af520",
+    "url": "/route-home.chunk.61f2c.js"
+  },
+  {
+    "revision": "61f2cd78d19fcd0af520",
+    "url": "/route-home.chunk.b2113.css"
+  },
+  {
+    "revision": "c42e15a55d7b98d811cc",
+    "url": "/route-resume.chunk.c42e1.js"
+  },
+  {
+    "revision": "c42e15a55d7b98d811cc",
+    "url": "/route-resume.chunk.d38c9.css"
+  },
+  {
+    "revision": "f87b8954386a5cbfcbe9",
+    "url": "/route-work.chunk.3dfe0.css"
+  },
+  {
+    "revision": "f87b8954386a5cbfcbe9",
+    "url": "/route-work.chunk.f87b8.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "f6e72d67052db524c7ee64526a817393",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
