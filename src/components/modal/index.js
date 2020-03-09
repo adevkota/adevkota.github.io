51955e8952a1454427523016f4b7c068fee2e6f8
Tosh <adevkota@pamperedchef.com>
2020-03-08 19:51:36 -0500

    carousel

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,11 +1,23 @@
 import { h } from "preact";
 import style from "./style";
+import { useReducer } from "preact/hooks";
 
-const Modal = ({onClose, showSelf, imageUrls, projectName, companyName, tech}) => {
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'increment': 
+			return state.imageIndexToDisplay === action.data ? {imageIndexToDisplay: 0} : {imageIndexToDisplay: state.imageIndexToDisplay + 1}
+		case 'decrement': 
+			return state.imageIndexToDisplay === 0 ? {imageIndexToDisplay: action.data - 1} : {imageIndexToDisplay: state.imageIndexToDisplay - 1};
+		default: return {imageIndexToDisplay: 0};
+	}
+};
+const Modal = ({ onClose, showSelf, imageUrls, projectName, companyName, tech }) => {
+	const [{imageIndexToDisplay}, dispatch] = useReducer(reducer, {imageIndexToDisplay: 0});
+	console.log("*******", imageIndexToDisplay);
 	return (
-		showSelf? 
+		showSelf ?
 			<div className={style.Modal} >
-				<div className={style.backdrop} onClick={() => {onClose()}}>
+				<div className={style.backdrop} onClick={() => { onClose() }}>
 				</div>
 				<div className={style.modalBox}>
 					<div className={style.modalHeader}>
@@ -13,8 +25,16 @@ const Modal = ({onClose, showSelf, imageUrls, projectName, companyName, tech}) =
 							<span class="icon-close"></span>
 						</div>
 					</div>
-					<div className={style.currentImage}>
-						<img src={imageUrls[0]} alt={`${projectName} demo image ${1}`}/>
+					<div className={style.imageCarousel}>
+						<div className={style.previousImage} onClick={() => dispatch({type: "decrement", data: imageUrls.length - 1})}>
+							<span class="icon-chevron-left" />
+						</div>
+						<div className={style.currentImage}>
+							<img src={imageUrls[imageIndexToDisplay]} alt={`${projectName} demo image ${1}`} />
+						</div>
+						<div className={style.nextImage} onClick={() => dispatch({type: "increment", data: imageUrls.length - 1})}>
+							<span class="icon-chevron-right" />
+						</div>
 					</div>
 					<div className={style.title}>
 						<span className={style.projectName}>{projectName}</span>
@@ -25,12 +45,12 @@ const Modal = ({onClose, showSelf, imageUrls, projectName, companyName, tech}) =
 							Built using: {tech}
 						</span>
 					</div>
-					
+
 				</div>
 			</div>
-		: ""
+			: ""
 	)
-	
+
 }
 
 export default Modal;
